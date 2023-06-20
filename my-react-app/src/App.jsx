2bde1e448ec33817fa7439dfe5fe088ded77c37d
decagon <chiorlujack@gmail.com>
2023-06-20 18:05:30 +0100

    updated

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,7 +5,6 @@ import Register from './assets/components/CreateAccount/Register'
 import Login from './assets/components/Login/Login'
 import VerifyUser from './assets/components/Login/Verify/VerifyUser'
 import Token from './assets/components/Login/Token/Token'
-import Dashboard from './assets/components/Dashboard/Dashboard'
 import ForgetPassword from './assets/components/Login/Forget-Password/ForgetPassword'
 import AuthenticatePassword from './assets/components/Login/Forget-Password/AuthenticatePassword'
 import EventDetails from './assets/components/EventDetails/EventDetails'
@@ -13,6 +12,7 @@ import Chekout from './assets/components/Chekout/Chekout'
 import Information from './assets/components/Dashboard/Account/Information'
 import PaymentSetup from './assets/components/Dashboard/Account/PaymentSetup'
 import TicketModel from './assets/components/TicketModel/TicketModel'
+import Event from './assets/components/Dashboard/Event'
 
 
 function App() {
@@ -28,12 +28,12 @@ function App() {
     <Route path='/user/forgetpassword/authenticate/' element={<ForgetPassword />} />
     <Route path='/verify-user/' element={<VerifyUser />} />
     <Route path="/token/:token" element={<Token />} />
-    <Route path="/app/dashboard" element={<Dashboard />} />
+    <Route path="/app/event" element={<Event />} />
     <Route path="/tokens" element={<AuthenticatePassword />} />
     <Route path="/chekout" element={<Chekout />} />
 
-    <Route path="/event-details" element={<EventDetails />} />
-    <Route path="/app/setuppayment" element={<PaymentSetup />} />
+    <Route path="/app/event-details" element={<EventDetails />} />
+    <Route path="/app/setup-account" element={<PaymentSetup />} />
     <Route path="/app/account" element={<Information />} />
     <Route path="/ticket" element={<TicketModel />} />
 
--- my-react-app/src/assets/components/Dashboard/CreateEvent/CreateEvent.jsx
+++ my-react-app/src/assets/components/Dashboard/CreateEvent/CreateEvent.jsx
@@ -3,6 +3,9 @@ const CreateEvent = ()=>{
     return (
         <>
         
+
+
+        
         </>
     )
 }
